41aa7303bcb5e49d90071b881d1e002ec07c9338
Prateek <saiprateekreddy96@gmail.com>
2021-04-26 11:04:49 -0400

    Completed JavaGrepImpl and LambdaStreamExcImpl apps

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpl.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpl.java
@@ -3,8 +3,14 @@ package ca.jrvs.apps.grep;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class JavaGrepImpl implements JavaGrep {
 
@@ -15,7 +21,7 @@ public class JavaGrepImpl implements JavaGrep {
   private String outFile;
 
   public static void main(String[] args) {
-    if (args.length!=3){
+    if (args.length != 3) {
       throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
     }
 
@@ -25,56 +31,84 @@ public class JavaGrepImpl implements JavaGrep {
 
   @Override
   public void process() throws IOException {
-
+    List<String> matchedLines = new ArrayList<>();
+    for (File file : listFiles(getRootPath())) {
+      for (String line : readLines(file)) {
+        if (containsPattern(getRegex())) {
+          matchedLines.add(line);
+        }
+      }
+    }
+  writeToFile(matchedLines);
   }
 
   @Override
   public List<File> listFiles(String rootDir) {
-    return null;
+    List<File> files = new ArrayList<File>();
+    File folder = new File(rootDir);
+    File[] AllFiles = folder.listFiles();
+    for (File f : AllFiles) {
+      if (f.isFile()) {
+        files.add(f);
+      }
+    }
+    return files;
   }
 
   @Override
   public List<String> readLines(File inputFile) {
-    return null;
+    Path path = Paths.get(inputFile.toURI());
+    List<String> lines = new ArrayList<>();
+    try {
+      lines = Files.readAllLines(path);
+    } catch (IOException e) {
+      logger.error("Unable to read lines from file", e);
+    }
+
+    return lines;
   }
 
   @Override
   public boolean containsPattern(String line) {
-    return false;
+    return Pattern.matches(getRegex(), line);
   }
 
   @Override
   public void writeToFile(List<String> lines) throws IOException {
-
+    FileWriter writer = new FileWriter(getOutFile());
+    for (String line : lines) {
+      writer.write(line + System.lineSeparator());
+    }
+    writer.close();
   }
 
   @Override
   public String getRootPath() {
-    return null;
+    return this.rootPath;
   }
 
   @Override
   public void setRootPath(String rootPath) {
-
+    this.rootPath = rootPath;
   }
 
   @Override
   public String getRegex() {
-    return null;
+    return this.regex;
   }
 
   @Override
   public void setRegex(String regex) {
-
+    this.regex = regex;
   }
 
   @Override
   public String getOutFile() {
-    return null;
+    return this.outFile;
   }
 
   @Override
   public void setOutFile(String outFile) {
-
+    this.outFile = outFile;
   }
 }
